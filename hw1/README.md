644fbad4ff9bc66bcc27cf7cfc4f686313f1caab
sarawang <369278452@qq.com>
2020-01-28 21:34:56 -0800

    hw1 - add testcase

--- hw1/README.md
+++ hw1/README.md
@@ -1,1 +1,2 @@
-	
+### TEST CASES are in main.scala:
+please refere to the variable **testcases** in main function	
--- hw1/main.scala
+++ hw1/main.scala
@@ -1,17 +1,12 @@
 import scala.meta._
 
+import scala.reflect.runtime.currentMirror
+import scala.tools.reflect.ToolBox
 object Main {
-	def getIntValue(node: Term): Int = {
-		node match {
-			case x: Lit.Int 	=> x.value
-			case _				=> throw new Exception("wrong type!")
-		}
-	}
-	def getBoolValue(node: Term): Boolean = {
-		node match {
-			case x: Lit.Boolean => x.value
-			case _				=> throw new Exception("wrong type!")
-		}
+	def eval(s: String) = {
+		val toolbox = currentMirror.mkToolBox()
+    	val tree = toolbox.parse(s)
+    	toolbox.eval(tree)
 	}
 	def step(node: Term): Term = {
 		node match {
@@ -20,15 +15,15 @@ object Main {
 			case x: Term.ApplyInfix => {
 				var left:Term = x.lhs
 				var right:Term = x.args(0)
-				if (left.isInstanceOf[Term.ApplyInfix]) {
+				if (left.isInstanceOf[Term.ApplyInfix] || left.isInstanceOf[Term.If]) {
 					return x.copy(lhs = step(left))
 				}
-				else if (right.isInstanceOf[Term.ApplyInfix]) {
+				else if (right.isInstanceOf[Term.ApplyInfix] || right.isInstanceOf[Term.If]) {
 					return x.copy(args = List(step(right)))
 				}
 				else if (left.isInstanceOf[Lit.Int] && right.isInstanceOf[Lit.Int]) {
-					var leftV = getIntValue(left)
-					var rightV = getIntValue(right)
+					var leftV = left.asInstanceOf[Lit.Int].value
+					var rightV = right.asInstanceOf[Lit.Int].value
 					if (x.op.value == "+") {
 						return Lit.Int(leftV + rightV)
 					} 
@@ -50,13 +45,14 @@ object Main {
 				}
 				else {
 					throw new Exception("incorrect type")
+					// return "Wrong Type"
 				}
 			}
     		case x: Term.If => {
-    			if (x.cond.isInstanceOf[Term.ApplyInfix]) {
+    			if (x.cond.isInstanceOf[Term.ApplyInfix] || x.cond.isInstanceOf[Term.If]) {
     				return x.copy(cond = step(x.cond))
     			} else if (x.cond.isInstanceOf[Lit.Boolean]) {
-    				if (getBoolValue(x.cond) == true) {
+    				if (x.cond.asInstanceOf[Lit.Boolean].value == true) {
     					return x.thenp
     				} else {
     					return x.elsep
@@ -73,17 +69,17 @@ object Main {
 
     def getType(node: Term): String = {
     	node match {
-    		case x: Lit.Int => "Int"
-    		case x: Lit.Boolean => "Boolean"
+    		case x: Lit.Int => "nat"
+    		case x: Lit.Boolean => "bool"
 			case x: Term.ApplyInfix => {
 				if (x.op.value == "+" || x.op.value == "*" || x.op.value == "<=") {
 					var left:Term = x.lhs
 					var right:Term = x.args(0)
-					if (getType(left) == "Int" && getType(right) == "Int") {
+					if (getType(left) == "nat" && getType(right) == "nat") {
 						if (x.op.value == "<=") {
-							return "Boolean"
+							return "bool"
 						} else {
-							return "Int"
+							return "nat"
 						}
 					} else {
 						return "Wrong Type"
@@ -94,43 +90,97 @@ object Main {
 				}
 			}
     		case x: Term.If => {
-    			if (getType(x.cond) == "Boolean") {
+    			if (getType(x.cond) == "bool") {
     				var a2 = getType(x.thenp)
     				var a3 = getType(x.elsep)
     				if (a2 == a3) {
     					return a2
     				} else {
-    					return "Wrong Type(thenp != elsep)"
+    					return "Wrong Type"
     				}
     			} else {
-    				return "Wrong Type in condition"
+    				return "Wrong Type"
     			}
     		}
     		case _          => {
-    			println("dunno")
-    			???
+    			return "Wrong Type"
     		}
     	}
     }
 
 	def main(args: Array[String]): Unit = {
-		var testcase = Array(
-			"true",
-			"false",
-			"1+2+3",
-			"if (1<=1) 2+3 else 4+5",
-			"if (true) 2+3 else 4+5",
-			"if (1<=1) true else false"
+		var testcases = Array(
+			Array("1", "nat"),
+			// bool
+			Array("true", "bool"),
+			Array("false", "bool"),
+			// n1+n2
+			Array("1+2", "nat"),
+			// n+a2
+			Array("1+(2+3)", "nat"),
+			Array("1+(2*3)", "nat"),
+			// a1+a2
+			Array("(1+2)+(3+4)", "nat"),
+			Array("(1*2)+(3*4)", "nat"),
+			Array("true+(3*4)", "Wrong Type"),
+			Array("true+false", "Wrong Type"),
+			// n1*n2
+			Array("1*2", "nat"),
+			// n*a2
+			Array("1*2*3", "nat"),
+			// a1*a2
+			Array("(1+1)*(2+3)", "nat"),
+			// n1<=n2
+			Array("1<=1", "bool"),
+			Array("1<=2", "bool"),
+			Array("2<=1", "bool"),
+			// n<=a2
+			Array("1<=(1+1)", "bool"),
+			// a1<=a2
+			Array("(2+1)<=(1+1)", "bool"),
+			// if true then a2 else a3
+			Array("if (true) 1 else 2", "nat"),
+			Array("if (true) 1 else false", "Wrong Type"),
+			Array("if (true) 2+3 else 4+5", "nat"),
+			// if false then a2 else a3
+			Array("if (false) 2+3 else 4+5", "nat"),
+			// if a1 then a2 else a3
+			Array("if (1<=2) 1 else 2", "nat"),
+			Array("if (2<=1) 1 else 2", "nat"),
+			Array("if (1<=1) 2+3 else 4+5", "nat"),
+			Array("if (1<=1) true else false", "bool"),
+			// mixed
+			Array("1 * (if (1<=2) 1 else 2)", "nat"),
+			Array("1 + (if (1<=2) true else false)", "Wrong Type"),
+			Array("(if (true) 1 else 2) + (if (1<=2) true else false)", "Wrong Type"),
+			Array("(if (true) 1 else 2) + (if (1<=2) 5 else 6)", "nat"),
+			Array("true + (if (1<=2) 5 else 6)", "Wrong Type"),
+			Array("if (1<=1) (if (1<=1) 2+3 else 4+5) else 6+7", "nat"),
+			Array("if (if (1<=1) true else false) (if (1<=1) 2+3 else 4+5) else 6+7", "nat"),
+			Array("if (if (if (if (1<=1) true else false) true else false) true else false) (if (1<=1) 2+3 else 4+5) else 6+7", "nat")
 		)
-		testcase.foreach(program => {
+
+		var i = 0
+		testcases.foreach(c => {
+			var program = c(0).asInstanceOf[String]
+			var typeResult = c(1).asInstanceOf[String]
 			var tree = program.parse[Term].get
-			println("-----------------------")
-			println("type:" + getType(tree))
-			while (!(tree.isInstanceOf[Lit.Int] || tree.isInstanceOf[Lit.Boolean])) {
-				println(tree.syntax)
-				tree = step(tree)
+			i += 1
+			println("-----------------------------------------")
+			println("Test Case " + i + " : " + program)
+			// 1. check type
+			println("Type is: " + getType(tree))
+			println("Type check: " + (if (typeResult == getType(tree)) "PASS***" else "FAIL!!!"))
+			if (getType(tree) != "Wrong Type") {
+				// 2. get value
+				println("Steps are:")
+				while (!(tree.isInstanceOf[Lit.Int] || tree.isInstanceOf[Lit.Boolean])) {
+					println("==> " + tree.syntax)
+					tree = step(tree)
+				}
+				println("==> " + tree.syntax)
+				println("Value check: " + (if (tree.syntax == eval(program).toString()) "PASS***" else "FAIL!!!"))
 			}
-			println("result:" + tree.syntax)
 		})
 	}
 }
